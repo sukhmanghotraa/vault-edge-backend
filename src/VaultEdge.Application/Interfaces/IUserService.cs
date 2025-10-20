8d3f61ad6f7de007c0009e57f7b809d8d6ba620f
Bangerr <devilism09@gmail.com>
2025-10-20 19:29:47 +0200

    Implement user-service interface

--- src/VaultEdge.Application/Interfaces/IUserService.cs
+++ src/VaultEdge.Application/Interfaces/IUserService.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VaultEdge.Domain.Entities;
 
 namespace VaultEdge.Application.Interfaces
 {
-    internal class IUserService
+    public interface IUserService
     {
+        Task<User?> GetUserName(Guid userId);
+        Task<string> GetUserNameAsync(Guid userId);
+        Task<User> GetUserById(Guid userId);
+        Task<bool> isUserActive(Guid userId);
+        Task<User> IsUserActiveAsync(Guid userId);
+        Task<IEnumerable<Guid>> GetUserRoleAsync(Guid userId);
+        Task<User> ValidateUserCredentialsAsync(string username, string password);
+        Task<Guid> CreateUserAsync(string username, string password, IEnumerable<Guid> roleIds);
+        Task<Guid> DeleteUserAsync(Guid userId);
+        Task<User> ActivateUserAsync(Guid userId);
+        Task<User> DeactiveUserAsync(Guid userId);
+        Task<User> ChangeUserPasswordAsync(Guid userId, string newPassword);
     }
 }
