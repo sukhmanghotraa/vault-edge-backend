888640a76307c28986c8c7c99c9dcbeecc5cf621
Bangerr <devilism09@gmail.com>
2025-10-26 16:41:33 +0100

    Revert "Temp commit"
    This reverts commit e41af19d2024d2bbda89f918aa353c6de8e40432.

--- src/VaultEdge.Application/Services/UserService.cs
+++ src/VaultEdge.Application/Services/UserService.cs
@@ -1,42 +1,12 @@
-﻿using VaultEdge.Application.Interfaces;
-using VaultEdge.Domain.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VaultEdge.Application.Services
 {
-    public class UserService: IUserService
+    internal class UserService
     {
-        private readonly IUserRepository _userRepository;
-        public UserService(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
-
-        public async Task<User> CreateUserAsync(CreateUserRequest request)
-        {
-            var user = new User(
-                request.FirstName,
-                request.LastName,
-                request.DateOfBirth,
-                request.TaxId,
-                request.IdentificationId,
-                request.Nationality,
-                request.Email,
-                request.PhoneNumber,
-                request.Address
-                );
-
-            await _userRepository.AddAsync(user);
-            return user;
-        }
-
-        public async Task<User?> GetUserByIdAsync(Guid id)
-        {
-            return await _userRepository.GetByIdAsync(id);
-        }
-
-        public async Task<User?> GetUserByEmailAsync(string email)
-        {
-            return await _userRepository.GetByEmailAsync(email);
-        }
     }
 }
