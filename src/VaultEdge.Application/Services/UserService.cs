e41af19d2024d2bbda89f918aa353c6de8e40432
Bangerr <devilism09@gmail.com>
2025-10-26 16:18:11 +0100

    Temp commit

--- src/VaultEdge.Application/Services/UserService.cs
+++ src/VaultEdge.Application/Services/UserService.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VaultEdge.Application.Interfaces;
+using VaultEdge.Domain.Entities;
 
 namespace VaultEdge.Application.Services
 {
-    internal class UserService
+    public class UserService: IUserService
     {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> CreateUserAsync(CreateUserRequest request)
+        {
+            var user = new User(
+                request.FirstName,
+                request.LastName,
+                request.DateOfBirth,
+                request.TaxId,
+                request.IdentificationId,
+                request.Nationality,
+                request.Email,
+                request.PhoneNumber,
+                request.Address
+                );
+
+            await _userRepository.AddAsync(user);
+            return user;
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid id)
+        {
+            return await _userRepository.GetByIdAsync(id);
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _userRepository.GetByEmailAsync(email);
+        }
     }
 }
