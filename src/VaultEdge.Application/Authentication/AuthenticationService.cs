8787f649648e0b7e2cd6db014886867f6f290145
Bangerr <devilism09@gmail.com>
2026-01-25 22:14:24 +0100

    Auth endpoints
    - Create auth endpoints
    - Model Signup, Signin request & response
    - Create auth service which return a mock response

---
+++ src/VaultEdge.Application/Authentication/AuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿namespace VaultEdge.Application.Authentication
+{
+    public class AuthenticationService: IAuthenticationService
+    {
+        public AuthenticationResult Signup(string firstName, string lastName, string email, string password)
+        {
+            return new AuthenticationResult(
+                Guid.NewGuid(), 
+                firstName, 
+                lastName, 
+                email, 
+                "token");
+        }
+
+        public AuthenticationResult Signin(string email, string password)
+        {
+            return new AuthenticationResult(
+                Guid.NewGuid(),
+                "firstName",
+                "lastName",
+                email,
+                "token");
+        }
+    }
+}
---
+++ src/VaultEdge.Application/Authentication/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿namespace VaultEdge.Application.Authentication
+{
+    public interface IAuthenticationService
+    {
+        AuthenticationResult Signup(string firstName, string lastName, string email, string password);
+
+        AuthenticationResult Signin(string email, string password);
+    }
+}
