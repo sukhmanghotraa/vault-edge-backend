28ec6f29563e70c34943abf207101fb976e79d40
Bangerr <devilism09@gmail.com>
2026-02-07 19:06:51 +0100

    Integrate Mapster for API mapping and refactor DI setup
    Added Mapster for object mapping in the API layer, including new mapping configuration classes for authentication flows. Refactored AuthenticationController to use Mapster's IMapper, replacing manual mapping logic. Introduced a DependencyInjection class for Mapster registration and consolidated service registrations into an AddPresentation extension method. Cleaned up Program.cs and updated project references to include Mapster packages.

---
+++ src/VaultEdge.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+using VaultEdge.Api.Authentication;
+using VaultEdge.Application.Authentication.Commands.Signup;
+using VaultEdge.Application.Authentication.Common;
+using VaultEdge.Application.Authentication.Queries.Signin;
+
+namespace VaultEdge.Api.Common.Mapping
+{
+    public class AuthenticationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<SignupRequest, SignupCommand>();
+
+            config.NewConfig<SigninRequest, SigninQuery>();
+
+            config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+                .Map(dest => dest.Token, src => src.Token)
+                .Map(dest => dest, src => src.User);
+        }
+    }
+}
---
+++ src/VaultEdge.Api/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using ErrorOr;
+using VaultEdge.Api.Common.Mapping;
+using VaultEdge.Api.Http;
+
+namespace VaultEdge.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowVaultEdgeFrontend",
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+
+        services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                var errors = context.HttpContext.Items[HttpContextItemKeys.Errors] as List<Error>;
+                if (errors is not null)
+                {
+                    context.ProblemDetails.Extensions.Add("errorCodes", errors.Select(e => e.Code));
+                }
+            };
+        });
+
+        //builder.Services.AddSingleton<ProblemDetailsFactory, VaultEdgeProblemDetailsFactory>();
+
+        services.AddMappings();
+
+        return services;
+    }
+}
