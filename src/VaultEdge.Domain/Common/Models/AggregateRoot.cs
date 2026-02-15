9f345136bbc0f138c19d3360307b9860987ea98b
Bangerr <devilism09@gmail.com>
2026-02-15 22:37:53 +0100

    Implement base classes for AggregateRoot, Entity, ValueObject

---
+++ src/VaultEdge.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace VaultEdge.Domain.Common.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
---
+++ src/VaultEdge.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,57 @@
+﻿namespace VaultEdge.Domain.Common.Models
+{
+    public abstract class ValueObject: IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+                return false;
+
+            var other = (ValueObject)obj;
+            return GetEqualityComponents()
+                .SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right) 
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals((object?)other);
+        }
+    }
+
+    public class Balance : ValueObject
+    {
+        public decimal Amount { get; private set; }
+        public string Burrency { get; private set; }
+
+        public Balance(decimal amount, string burency)
+        {
+            Amount = amount;
+            Burrency = burency;
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Amount;
+            yield return Burrency;
+        }
+    }
+}
