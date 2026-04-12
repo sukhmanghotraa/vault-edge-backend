05a9c62a6148343a085f17b621896a18a046d536
Bangerr <devilism09@gmail.com>
2026-04-12 19:42:37 +0200

    Add GetAccountsByCustomerId query and endpoint
    Implement GetAccountsByCustomerIdQuery and its handler to fetch all accounts for a specific customer. Replace the GetAllAccounts endpoint with a new GET endpoint at all/{customerId:guid} in AccountsController. Use ErrorOr for error handling and return AccountDto list or appropriate error if none found. Add necessary using statements.

---
+++ src/VaultEdge.Application/Accounts/Queries/GetAccountsByCustomerId/GetAccountsByCustomerIdHandler.cs
@@ -0,0 +1,41 @@
+﻿using ErrorOr;
+using VaultEdge.Application.Abstractions;
+using VaultEdge.Application.Accounts.DTOs;
+using VaultEdge.Application.Accounts.Queries.GetAllAccounts;
+using VaultEdge.Application.Repositories;
+using VaultEdge.Domain.Common.Errors;
+
+namespace VaultEdge.Application.Accounts.Queries.GetAccountsByCustomerId
+{
+    public class GetAccountsByCustomerIdHandler : IQueryHandler<GetAccountsByCustomerIdQuery, List<AccountDto>>
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public GetAccountsByCustomerIdHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<ErrorOr<List<AccountDto>>> Handle(GetAccountsByCustomerIdQuery request, CancellationToken cancellationToken = default)
+        {
+            var accounts = await _accountRepository.GetByCustomerIdAsync(request.CustomerId);
+
+            if (accounts is null)
+            {
+                return AccountErrors.Account.NoneFound;
+            }
+
+            var accountsCopy = accounts.Select(account => new AccountDto
+            {
+                Id = account.Id,
+                AccountNumber = account.AccountNumber,
+                CustomerId = account.CustomerId,
+                AccountType = account.Type,
+                Transactions = account.Transactions,
+                CreatedAt = account.CreatedAt
+            });
+
+            return accountsCopy.ToList();
+        }
+    }
+}
